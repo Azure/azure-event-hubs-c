458375fa4755370dd05fa9c5898c3b81e5bec9b3
Dan Cristoloveanu <dcristo@microsoft.com>
2019-03-10 21:36:27 -0700

    Extract version unit tests

---
+++ eventhub_client/tests/version_ut/version_ut.c
@@ -0,0 +1,74 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifdef __cplusplus
+#include <cstring>
+#else
+#include <string.h>
+#endif
+
+#include "testrunnerswitcher.h"
+#include "umock_c.h"
+
+#include "version.h"
+
+static TEST_MUTEX_HANDLE g_testByTest;
+
+DEFINE_ENUM_STRINGS(UMOCK_C_ERROR_CODE, UMOCK_C_ERROR_CODE_VALUES)
+
+static void on_umock_c_error(UMOCK_C_ERROR_CODE error_code)
+{
+    ASSERT_FAIL("umock_c reported error :%s", ENUM_TO_STRING(UMOCK_C_ERROR_CODE, error_code));
+}
+
+BEGIN_TEST_SUITE(version_unittests)
+
+TEST_SUITE_INITIALIZE(TestClassInitialize)
+{
+    int result;
+
+    g_testByTest = TEST_MUTEX_CREATE();
+    ASSERT_IS_NOT_NULL(g_testByTest);
+
+    result = umock_c_init(on_umock_c_error);
+    ASSERT_ARE_EQUAL(int, 0, result);
+}
+
+TEST_SUITE_CLEANUP(TestClassCleanup)
+{
+    umock_c_deinit();
+
+    TEST_MUTEX_DESTROY(g_testByTest);
+}
+
+TEST_FUNCTION_INITIALIZE(TestMethodInitialize)
+{
+    if (TEST_MUTEX_ACQUIRE(g_testByTest))
+    {
+        ASSERT_FAIL("our mutex is ABANDONED. Failure in test framework");
+    }
+
+    umock_c_reset_all_calls();
+}
+
+TEST_FUNCTION_CLEANUP(TestMethodCleanup)
+{
+    TEST_MUTEX_RELEASE(g_testByTest);
+}
+
+/*** EventHubClient_GetVersionString ***/
+
+/* Tests_SRS_VERSION_01_001: [EventHubClient_GetVersionString shall return a pointer to a constant string which indicates the version of EventHubClient API.] */
+TEST_FUNCTION(EventHubClient_GetVersionString_Success)
+{
+    // arrange
+
+    // act
+    const char* result = EventHubClient_GetVersionString();
+
+    // assert
+    ASSERT_IS_NOT_NULL(result);
+    ASSERT_ARE_EQUAL(char_ptr, umock_c_get_expected_calls(), umock_c_get_actual_calls());
+}
+
+END_TEST_SUITE(version_unittests)
